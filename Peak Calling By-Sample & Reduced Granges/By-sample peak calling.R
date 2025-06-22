af5a483947f44130507a75212ad81ddc06d61cf9
Yulong0329 <qiuy936@gmail.com>
2025-06-21 22:09:14 -0700

    Add files via upload

---
+++ Peak Calling By-Sample & Reduced Granges/By-sample peak calling.R
@@ -0,0 +1,31 @@
+library(Signac)
+#File path
+fragpath <- "/Users/yulongqiu/Desktop/Biostats/Master_Thesis/Sample/GSM6058856_B004-A-004-D_20200817_fragments.tsv.gz"
+file.exists(fragpath)
+
+#See the first five lines of the fragment file
+head(read_tsv(fragpath, col_names = F, n_max = 5))
+#col_names函数告诉 read_tsv()：文件第一行不是列名，而是数据。
+#n_max只读取文件的 前 5 行数据。
+
+#Count the number of fragments per cell in the fragment file
+require(Signac)
+total_counts <- CountFragments(fragpath)
+
+#Screening cell barcodes with more than 100 fragments
+barcodes <- total_counts$CB[total_counts$frequency_count > 100]
+
+#Create a fragment object
+frags <- Signac::CreateFragmentObject(path=fragpath, cells=barcodes)
+
+#peak calling
+#peak calling for MACS3 in terminal
+system('bash -c "/Users/yulongqiu/macs3_env/bin/macs3 callpeak -t /Users/yulongqiu/Desktop/Biostats/Master_Thesis/Sample/GSM6058856_B004-A-004-D_20200817_fragments.tsv.gz -g 2.7e+09 -f BED --nomodel --extsize 200 --shift -100 -n SeuratProject --outdir /Users/yulongqiu/Desktop/Biostats/Master_Thesis/peaks"')
+
+
+
+
+
+
+
+
