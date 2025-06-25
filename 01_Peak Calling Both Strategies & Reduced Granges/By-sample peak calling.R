acd67889f54a4995fc9b15f5b876b044a7b0373a
Yulong0329 <qiuy936@gmail.com>
2025-06-24 17:07:23 -0700

    Add files via upload

---
+++ 01_Peak Calling Both Strategies & Reduced Granges/By-sample peak calling.R
@@ -0,0 +1,12 @@
+library(Signac)
+#peak calling
+#peak calling for MACS3 in terminal
+system('bash -c "/Users/yulongqiu/macs3_env/bin/macs3 callpeak -t /Users/yulongqiu/Desktop/Biostats/Master_Thesis/Sample/GSM6058856_B004-A-004-D_20200817_fragments.tsv.gz -g 2.7e+09 -f BED --nomodel --extsize 200 --shift -100 -n SeuratProject --outdir /Users/yulongqiu/Desktop/Biostats/Master_Thesis/peaks"')
+
+
+
+
+
+
+
+
